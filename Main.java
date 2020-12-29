cd46f37de059476843344623fbf55a2af1aad4fc
Sfunzer <75363441+Sfunzer@users.noreply.github.com>
2020-12-29 14:56:56 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+    static String weGaanPrinten;
+    static Printer deskJet1;
+
+
+    public static void main(String[] args) {
+        deskJet1 = new Printer();
+
+        weGaanPrinten = deskJet1.printTask1();
+
+        System.out.println(weGaanPrinten);
+
+
+    }
+}
---
+++ Printer.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Printer {
+    final private String backSender;
+
+
+    public Printer() {
+        backSender = "Print print print print print blablalba";
+    }
+
+    public String printTask1 () {
+        return  backSender;
+    }
+
+}
